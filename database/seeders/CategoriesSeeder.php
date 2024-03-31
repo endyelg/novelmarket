29e2127b18e031b1c5307f5e36a5a54455fb91dd
endyelg <galaponangelrubia@gmail.com>
2024-04-01 02:33:31 +0800

    bago

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -16,17 +16,13 @@ class CategoriesSeeder extends Seeder
             ['title' => 'Fantasy', 'slug' => 'Fantasy'],
             ['title' => 'Mystery', 'slug' => 'Mystery'],
             ['title' => 'Thriller', 'slug' => 'Thriller'],
-            ['title' => 'Horror', 'slug' => 'Horror'],
             ['title' => 'Biography', 'slug' => 'Biography'],
             ['title' => 'History', 'slug' => 'History'],
             ['title' => 'Philosophy', 'slug' => 'Philosophy'],
-            ['title' => 'Travel', 'slug' => 'Travel'],
             ['title' => 'Poetry', 'slug' => 'Poetry'],
-            ['title' => 'Business', 'slug' => 'Business'],
             ['title' => 'Art', 'slug' => 'Art'],
             ['title' => 'Music', 'slug' => 'Music'],
-            ['title' => 'Health', 'slug' => 'Health'],
-            ['title' => 'Fitness', 'slug' => 'Fitness'],
+
 
             // Add more categories as needed
         ];
